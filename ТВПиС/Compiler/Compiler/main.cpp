4168109b2f62187ff9edf46d7fed9ab213a7ab63
Василий <artamonovvasya10@gmail.com>
2018-05-14 16:16:06 +0300

    Visual Studio

--- ТВПиС/Compiler/Compiler/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
-{
-    MessageBox(NULL, "Hi meganewbie!", "My Hero", MB_OK);
-    return 0;
-}
